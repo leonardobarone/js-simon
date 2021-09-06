d21889055c54b9e33503d4e9d94a507347e0e79f
leonardobarone <leonardobarone26@gmail.com>
2021-09-06 15:48:36 +0200

    / Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.

--- js/script.js
+++ js/script.js
@@ -4,20 +4,31 @@ for (var i = 0; i < 5; i++) {
     var numeroRandom = Math.floor(Math.random() * 5 + 1);
     arrayDiNumeriRandom.push(numeroRandom);
 }
-console.log(`I 5 numeri generati casualmente sono: ${arrayDiNumeriRandom.join(' - ')}`);
+alert(`I 5 numeri generati casualmente sono: ${arrayDiNumeriRandom.join(' - ')}`);
 // Da li parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 var arrayNumeriInseritiDaUtente = [];
 setTimeout(
     function(){
-        numeroInseritoDaUtente = parseInt(prompt("Inserisci i numeri che hai visto precendemente"));
         for(var i = 0; i < 5; i++) {
+            numeroInseritoDaUtente = parseInt(prompt("Inserisci i numeri che hai visto precendemente"));
             arrayNumeriInseritiDaUtente.push(numeroInseritoDaUtente);
         }
     },
     3 * 1000
 );
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-for (var i = 0; i < 5; i++) {
-   console.log(arrayNumeriInseritiDaUtente[i])
-}
+setTimeout (
+    function(){
+        var control = false;
+        for (var i = 0; i < 5; i++) {
+           if (arrayDiNumeriRandom[i] == arrayNumeriInseritiDaUtente[i]) {
+                control = true;
+           }
+         }
+         if (control == true) {
+             alert("hai vinto!")
+         }
+    },
+    4 * 1000
+);
