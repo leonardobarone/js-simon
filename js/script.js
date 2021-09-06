e986de1fb8de7cbbbd26284482e09ca929d05943
leonardobarone <leonardobarone26@gmail.com>
2021-09-06 15:03:21 +0200

    Da li parte un time di 30 secondi

--- js/script.js
+++ js/script.js
@@ -6,5 +6,9 @@ for (var i = 0; i < 5; i++) {
 }
 alert(`I 5 numeri generati casualmente sono: ${arrayDiNumeriRandom.join(' - ')}`);
 // Da li parte un timer di 30 secondi.
+setTimeout(
+    function(){console.log("ciao")},
+    3 * 1000
+);
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
