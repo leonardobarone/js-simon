02ccd4f56859828452f8063cde5eaaf38dc3649e
leonardobarone <leonardobarone26@gmail.com>
2021-09-06 15:37:33 +0200

    richiesta hotwell

--- js/script.js
+++ js/script.js
@@ -4,11 +4,20 @@ for (var i = 0; i < 5; i++) {
     var numeroRandom = Math.floor(Math.random() * 5 + 1);
     arrayDiNumeriRandom.push(numeroRandom);
 }
-alert(`I 5 numeri generati casualmente sono: ${arrayDiNumeriRandom.join(' - ')}`);
+console.log(`I 5 numeri generati casualmente sono: ${arrayDiNumeriRandom.join(' - ')}`);
 // Da li parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+var arrayNumeriInseritiDaUtente = [];
 setTimeout(
-    function(){console.log("ciao")},
+    function(){
+        numeroInseritoDaUtente = parseInt(prompt("Inserisci i numeri che hai visto precendemente"));
+        for(var i = 0; i < 5; i++) {
+            arrayNumeriInseritiDaUtente.push(numeroInseritoDaUtente);
+        }
+    },
     3 * 1000
 );
-// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+for (var i = 0; i < 5; i++) {
+   console.log(arrayNumeriInseritiDaUtente[i])
+}
